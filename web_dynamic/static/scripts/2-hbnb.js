95d0674a05eef60587be14f651648cdae431a52d
Caroll1889 <diahancaroll@hotmail.com>
2020-02-18 19:52:32 +0000

    Fixed some functions

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,27 @@
-$(document).ready(function () {
-  const AmeId = {};
+const AmeId = {};
+
+function checkAm() {
+  const allAmen = $('.amenities INPUT');
+  allAmen.each(function () {
+    $(this).change(function () {
+      if ($(this).prop('checked')) { 
+        AmeId[$(this).data('name')] = $(this).data('id');
+      } else {
+          delete AmeId[$(this).data('name')];
+      }
+      const TagH4 = $(this).closest('.amenities').find('h4');
+      const lista = Object.keys(AmeId);
+      if (lista.length > 0) {
+        TagH4.text(lista);
+        console.log(lista);
+       } else {
+          TagH4.text(' ');
+       }
+     });
+  });
+});
+
+function checkStatus() {
 
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/status/',
@@ -17,19 +39,10 @@ $(document).ready(function () {
       $('DIV#api_status').removeClass('available');
     }
   });
+}
 
-  $('input[type=ckeckbox]').click(function () {
-    if ($(this).is(':ckecked')) {
-      AmeId[$(this).data('name')] = $(this).data('id');
-    } else {
-      delete AmeId[$(this).data('name')];
-    }
-    const TagH4 = $(this).closest('.amenities').find('h4');
-    const lista = Object.keys(AmeId);
-    if (lista.length > 0) {
-      TagH4.text(lista);
-    } else {
-      TagH4.text('');
-    }
-  });
+
+$(document).ready(function () {
+  checkAm();
+  checkStatus();
 });
